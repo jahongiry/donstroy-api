124d111b1afcb8dcb50b054ca0931e734ea9c459
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2024-03-24 11:26:12 +0500

    Add login register jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   devise_for :users
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+# end
+
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'sessions#create'
+    end
+  end
 end

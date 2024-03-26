5bfd526326d39b545adf5c0e4edb7fc44b0dc1f6
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2024-03-26 13:00:39 +0500

    Add cors and courses

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create]
       post '/login', to: 'sessions#create'
-      resources :courses # Add this line to include courses resources
+      resources :courses 
       resources :students
     end
   end

404baf3c61156738c0ea3d7a997a83ab8aada04b
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2024-03-29 10:56:50 +0500

    Add maindesign

--- app/models/student.rb
+++ app/models/student.rb
@@ -72,59 +72,102 @@ class Student < ApplicationRecord
       c.draw "text #{name_start_x },#{name_y} '#{escaped_name.upcase}'"
     end
 
-    line1_x = (image.width - 200) / 2 - 10
+
+    line1_x = (image.width - 200) / 2 - 15
     line1_y = (image.width - 200) / 2 + 35
     image.combine_options do |c|
       c.font Rails.root.join('public', 'arial.ttf')
       c.fill "black"
-      c.pointsize 14  
-      c.draw "text #{line1_x},#{line1_y} '\"DON STROY PROJECT\" MCHJ ning Kadrlar malakasini oshirish '"
+      c.pointsize 13  
+      c.draw "text #{line1_x},#{line1_y} '\"DON STROY PROJECT\" MCHJning kadrlar malakasini oshirish bo\\'limida '"
     end
 
-    line2_x = (image.width - 200) / 2 - 20
-    line2_y = (image.width - 200) / 2 + 55
-    image.combine_options do |c|
-      c.fill "black"
-      c.pointsize 14
+    # Calculate the starting position for the course name
+    course_name_x = (image.width - 200) / 2
+    course_name_y = (image.width - 200) / 2 + 55
 
-      # Draw the non-bold part of the text
-      c.font Rails.root.join('public', 'arial.ttf')
-      c.draw "text #{line2_x},#{line2_y} 'va sertifikatlash bo\\'limida '"
+    if course.name.length > 55
+      # Split the course name into two lines
+      first_line = course.name[0..49]
+      second_line = course.name[50..-1]
 
-      # Draw the bold part of the text (course name)
-      c.font Rails.root.join('public', 'arial_bold.ttf')
-      c.draw "text #{line2_x + 180},#{line2_y} '#{course.name}'"
+      # Calculate the starting position for the first line
+      first_line_length = first_line.length
+      half_first_line_width = (first_line_length * 10) / 2  
+      first_line_start_x = (center_x + 70) - half_first_line_width
+
+      # Calculate the starting position for the second line
+      second_line_length = second_line.length
+      half_second_line_width = (second_line_length * 10) / 2  
+      second_line_start_x = (center_x + 70) - half_second_line_width
+
+      # Draw the first line
+      image.combine_options do |c|
+        c.fill "black"
+        c.pointsize 14
+        c.font Rails.root.join('public', 'arial.ttf')
+        c.font Rails.root.join('public', 'arial_bold.ttf')
+        c.draw "text #{first_line_start_x},#{course_name_y} '#{first_line.gsub("'", "\\\\'")}'"
+      end
+
+      # Draw the second line
+      image.combine_options do |c|
+        c.fill "black"
+        c.pointsize 14
+        c.font Rails.root.join('public', 'arial.ttf')
+        c.font Rails.root.join('public', 'arial_bold.ttf')
+        c.draw "text #{second_line_start_x},#{course_name_y + 20} '#{second_line.gsub("'", "\\\\'")}'"
+      end
+    else
+      # If the course name's length is within 50 characters, draw it on a single line
+      course_length = course.name.length
+      half_course_width = (course_length * 10) / 2
+      course_start_x = (center_x + 30) - half_course_width
+      escaped_course = course.name.gsub("'", "\\\\'")
+      image.combine_options do |c|
+        c.fill "black"
+        c.pointsize 14
+        c.font Rails.root.join('public', 'arial.ttf')
+        c.font Rails.root.join('public', 'arial_bold.ttf')
+        c.draw "text #{course_start_x},#{course_name_y} '#{escaped_course}'"
+      end
     end
 
-    line3_x = (image.width - 200) / 2 + 5
-    line3_y = (image.width - 200) / 2 + 75
-    image.combine_options do |c|
-      c.fill "black"
-      c.pointsize 14
-      c.font Rails.root.join('public', 'arial.ttf')
-      c.draw "text #{line3_x},#{line3_y} 'bo\\'yicha 72 soatlik soxaviy mavzularini o\\'qidi, yakuniy'"
+    if course.name.length > 65
+      next_line = 0
+    else
+      next_line = 20
     end
 
-    line4_x = (image.width - 200) / 2 - 5
-    line4_y = (image.width - 200) / 2 + 95
+    line3_x = (image.width - 200) / 2 - 13
+    line3_y = (image.width - 200) / 2 + 55 + next_line
     image.combine_options do |c|
       c.fill "black"
-      c.pointsize 14
+      c.pointsize 13
       c.font Rails.root.join('public', 'arial.ttf')
-      c.draw "text #{line4_x},#{line4_y} 'baholashni ijobiy topshirganligi uchun \"Mualliflik nazorati\"'"
+      c.draw "text #{line3_x},#{line3_y} 'bo\\'yicha #{hour} soatlik soxaviy mavzularini o\\'qidi, yakuniy baholashni ijobiy'"
     end
 
-    line5_x = (image.width - 200) / 2 + 100
-    line5_y = (image.width - 200) / 2 + 115
+    line4_x = (image.width - 200) / 2 
+    line4_y = (image.width - 200) / 2 + 75 + next_line
     image.combine_options do |c|
       c.fill "black"
-      c.pointsize 14
+      c.pointsize 13
       c.font Rails.root.join('public', 'arial.ttf')
-      c.draw "text #{line5_x},#{line5_y} 'bo\\'yicha sertifikat berildi'"
+      c.draw "text #{line4_x},#{line4_y} 'topshirganligi uchun \"#{control}\" bo\\'yicha sertifikat berildi'"
+    end
+
+    level_x = (image.width - 200) / 2 + 160
+    level_y = (image.width - 200) / 2 + 135
+    image.combine_options do |c|
+      c.fill "black"
+      c.pointsize 16
+      c.font Rails.root.join('public', 'arial.ttf')
+      c.draw "text #{level_x},#{level_y} #{level}"
     end
 
     line6_x = (image.width - 200) / 2 + 115
-    line6_y = (image.width - 200) / 2 + 170
+    line6_y = (image.width - 200) / 2 + 185
     image.combine_options do |c|
       c.fill "black"
       c.pointsize 13
@@ -133,9 +176,9 @@ class Student < ApplicationRecord
     end
 
     certificate_date_x = (image.width - 200) / 2 + 140
-    certificate_date_y = image.height - 50
+    certificate_date_y = image.height - 40
     expiration_date_x = (image.width - 200) / 2  + 140
-    expiration_date_y = image.height - 30
+    expiration_date_y = image.height - 20
     image.combine_options do |c|
       c.font Rails.root.join('public', 'arial.ttf')
       c.fill "black"
